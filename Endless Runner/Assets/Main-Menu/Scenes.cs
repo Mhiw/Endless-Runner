fd16291f765cba7625e092a1be8aeb9a4b1bac88
JoelSuperHacker <09.joelandersson@gmail.com>
2024-03-29 22:05:11 +0100

    Added option scene + basic for option scene

--- Endless Runner/Assets/Main-Menu/Scenes.cs
+++ Endless Runner/Assets/Main-Menu/Scenes.cs
@@ -9,4 +9,22 @@ public class Scenes : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
 }
